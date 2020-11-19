d70393cb5169ee696d24b220aa5607323de938ef
willow123red <45798084+willow123red@users.noreply.github.com>
2020-11-18 22:08:31 -0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { ListComponent } from './list/list.component';
+import { ItemComponent } from './item/item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TabsComponent,
+    ListComponent,
+    ItemComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

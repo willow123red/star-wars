df17a63b56ab5b25eb17c5fd51a403750034bf4b
willow123red <45798084+willow123red@users.noreply.github.com>
2020-11-18 22:46:37 -0700

    initiate list and item components

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
-
+  character;
+  
   constructor() { }
 
   ngOnInit() {
